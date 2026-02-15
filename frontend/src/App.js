9095e99275e8dd3fa49b61d00ebe22e15917c2ac
Enoch Eesa <115502485+EnochEesa@users.noreply.github.com>
2026-02-15 15:49:52 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [msg, setMsg] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/hello")
+      .then(res => res.json())
+      .then(data => setMsg(data.message));
+  }, []);
+
+  return (
+    <div>
+      <h1>Frontend</h1>
+      <p>{msg}</p>
+    </div>
+  );
+}
+
+export default App;
